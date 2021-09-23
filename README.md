72ccecea2274c660dabf951a6fee65d6f880c78d
TakumuTakanezawa <takanen20010829kota@gmail.com>
2021-09-23 13:32:13 +0900

    add テスト2

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ This is a slide published by Takanen.
 
 ### List of slides
 
+* [テスト2](../slide/テスト2/)
 * [test](../slide/テスト/)
--- add.sh
+++ add.sh
@@ -1,5 +1,20 @@
-git add .
-echo "gitにpushするコメントを入力してください。(半角スペースは禁止)"
-read COMMENT
-git commit -m $COMMENT
-git push origin main
+if [ -e export ]
+then 
+    # exportフォルダを改名
+    echo "スライドのタイトルを入力してください。(半角スペース禁止)"
+    read TITLE
+    mv -f export/ $TITLE
+    # read meにリンクを追加
+    sed -i "6i * [${TITLE}](../slide/${TITLE}/)" README.md
+    # git に commit and push
+    git add .
+    git commit -m "add ${TITLE}"
+    git push origin main
+else
+    # git に commit and push
+    git add .
+    echo "gitにpushするコメントを入力してください。(半角スペースは禁止)"
+    read COMMENT
+    git commit -m $COMMENT
+    git push origin main
+fi
