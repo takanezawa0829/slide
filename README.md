944d41ffe2495e66dc0f93e7ae71bb53119b90d2
TakumuTakanezawa <takanen20010829kota@gmail.com>
2021-09-23 13:52:37 +0900

    add テスト

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This is a slide published by Takanen.
 ### List of slides
 
 * [テスト](../slide/テスト/)
+* [テスト](../slide/テスト/)
 * [test](../slide/test/)
 * [テスト](../slide/テスト/)
 * [test](../slide/test/)
--- add.sh
+++ add.sh
@@ -3,9 +3,9 @@ then
     # exportフォルダを改名
     echo "スライドのタイトルを入力してください。(半角スペース禁止)"
     read TITLE
-    if [ -e "${TITLE}"]
+    if [ -e $TITLE]
     then
-        rm -rf "${TITLE}"
+        rm -rf $TITLE
     fi
     # mv -f export/ $TITLE
     # read meにリンクを追加
